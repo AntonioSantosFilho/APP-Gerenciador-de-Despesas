cd2b14cf17e754f6de40da86168ffb6c68e10aed
AntonioSantosFilho <antonio.santosfilho@discente.univasf.edu.br>
2024-02-16 02:00:37 -0300

    Component Chart

---
+++ lib/components/chart.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../models/transaction.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransaction;
+  const Chart(this.recentTransaction, {super.key});
+
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+
+      return {'day': DateFormat.E().format(weekDay)[0], 'value': 9.99};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(),
+    );
+  }
+}
